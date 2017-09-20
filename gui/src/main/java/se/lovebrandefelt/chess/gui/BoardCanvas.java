5948cec8e4b7e92ec15df741ef09a83eb04c73e1
lbrande <lovbra00@gmail.com>
2017-09-20 15:55:50 +0200

    Setup drawing of chess board.

--- gui/src/main/java/se/lovebrandefelt/chess/gui/BoardCanvas.java
+++ gui/src/main/java/se/lovebrandefelt/chess/gui/BoardCanvas.java
@@ -1,8 +1,51 @@
 package se.lovebrandefelt.chess.gui;
 
+import static javafx.scene.paint.Color.ANTIQUEWHITE;
+import static javafx.scene.paint.Color.LIGHTSEAGREEN;
+import static se.lovebrandefelt.chess.gui.GUI.IMAGES;
+
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import se.lovebrandefelt.chess.Board;
+import se.lovebrandefelt.chess.Piece;
+import se.lovebrandefelt.chess.Pos;
 
 public class BoardCanvas extends Canvas {
+  private static final Color WHITE_SQUARE_COLOR = ANTIQUEWHITE;
+  private static final Color BLACK_SQUARE_COLOR = LIGHTSEAGREEN;
   private Board board;
+  private double squareSize;
+
+  public void setBoard(Board board) {
+    this.board = board;
+    squareSize = getWidth() / board.cols();
+    draw();
+  }
+
+  public void draw() {
+    if (board != null) {
+      GraphicsContext graphicsContext = getGraphicsContext2D();
+      for (int row = 0; row < board.rows(); row++) {
+        for (int col = 0; col < board.cols(); col++) {
+          if ((row + col) % 2 == 0) {
+            graphicsContext.setFill(WHITE_SQUARE_COLOR);
+          } else {
+            graphicsContext.setFill(BLACK_SQUARE_COLOR);
+          }
+          graphicsContext.fillRect(col * squareSize, row * squareSize, squareSize, squareSize);
+          Pos pos = new Pos(row, col);
+          if (!board.isEmpty(pos)) {
+            Piece piece = board.get(pos);
+            graphicsContext.drawImage(
+                IMAGES.get(piece.getColor()).get(piece.getTypeId()),
+                col * squareSize,
+                row * squareSize,
+                squareSize,
+                squareSize);
+          }
+        }
+      }
+    }
+  }
 }
--- gui/src/main/java/se/lovebrandefelt/chess/gui/GUI.java
+++ gui/src/main/java/se/lovebrandefelt/chess/gui/GUI.java
@@ -1,24 +1,53 @@
 package se.lovebrandefelt.chess.gui;
 
+import static se.lovebrandefelt.chess.Color.BLACK;
+import static se.lovebrandefelt.chess.Color.WHITE;
+import static se.lovebrandefelt.chess.Game.standardSetup;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import se.lovebrandefelt.chess.Color;
 
 public class GUI extends Application {
+  public static final Map<Color, Map<Character, Image>> IMAGES = new HashMap<>();
 
   public static void main(String[] args) {
     launch(args);
   }
 
   @Override
+  public void init() {
+    IMAGES.put(WHITE, new HashMap<>());
+    IMAGES.get(WHITE).put('B', new Image("WHITE_B.png"));
+    IMAGES.get(WHITE).put('K', new Image("WHITE_K.png"));
+    IMAGES.get(WHITE).put('N', new Image("WHITE_N.png"));
+    IMAGES.get(WHITE).put('P', new Image("WHITE_P.png"));
+    IMAGES.get(WHITE).put('Q', new Image("WHITE_Q.png"));
+    IMAGES.get(WHITE).put('R', new Image("WHITE_R.png"));
+    IMAGES.put(BLACK, new HashMap<>());
+    IMAGES.get(BLACK).put('B', new Image("BLACK_B.png"));
+    IMAGES.get(BLACK).put('K', new Image("BLACK_K.png"));
+    IMAGES.get(BLACK).put('N', new Image("BLACK_N.png"));
+    IMAGES.get(BLACK).put('P', new Image("BLACK_P.png"));
+    IMAGES.get(BLACK).put('Q', new Image("BLACK_Q.png"));
+    IMAGES.get(BLACK).put('R', new Image("BLACK_R.png"));
+  }
+
+  @Override
   public void start(Stage primaryStage) throws IOException {
     Parent root = FXMLLoader.load(getClass().getResource("/GUI.fxml"));
-    Scene scene = new Scene(root, 600, 400);
+    Scene scene = new Scene(root);
+    ((BoardCanvas) scene.lookup("#board")).setBoard(standardSetup());
     primaryStage.setScene(scene);
     primaryStage.setTitle("Chess");
     primaryStage.show();
+    primaryStage.sizeToScene();
   }
 }
