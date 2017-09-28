ccfd6429cc00de28c5f4873bbbba5a0678282bc0
Filip Lindvall <drpytho@gmail.com>
2017-09-28 19:47:59 +0200

    #3 Added test for checkmate should en the game with correct winner. Added gitignore file

--- src/test/java/se/lovebrandefelt/chess/GameTest.java
+++ src/test/java/se/lovebrandefelt/chess/GameTest.java
@@ -31,6 +31,39 @@ class GameTest {
   }
 
   @Test
+  void checkmateShouldPutGameToWinner() {
+    // A simple checkmate setup
+    board = new Board(8, 8);
+    board.add(new King(WHITE), new Pos(0, 0));
+    board.add(new Rook(WHITE), new Pos(6, 1));
+    board.add(new Rook(WHITE), new Pos(7, 0));
+    board.add(new King(BLACK), new Pos(7, 6));
+    game = new Game(board, BLACK);
+    assertEquals(Game.State.WHITE_WON, game.state());
+
+    // D. Byrne vs. Fischer checkmate
+    board = new Board(8, 8);
+    board.add(new Queen(WHITE), new Pos(0, 1));
+    board.add(new Pawn(BLACK), new Pos(1, 5));
+    board.add(new King(BLACK), new Pos(1, 6));
+    board.add(new Pawn(BLACK), new Pos(2, 2));
+    board.add(new Pawn(BLACK), new Pos(2, 6));
+    board.add(new Pawn(BLACK), new Pos(3, 1));
+    board.add(new Knight(WHITE), new Pos(3, 4));
+    board.add(new Pawn(BLACK), new Pos(3, 7));
+    board.add(new Bishop(BLACK), new Pos(4, 1));
+    board.add(new Pawn(WHITE), new Pos(4, 7));
+    board.add(new Bishop(BLACK), new Pos(5, 1));
+    board.add(new Knight(BLACK), new Pos(5, 2));
+    board.add(new Rook(BLACK), new Pos(6, 2));
+    board.add(new Pawn(WHITE), new Pos(6, 6));
+    board.add(new King(WHITE), new Pos(7, 2));
+
+    game = new Game(board, WHITE);
+    assertEquals(Game.State.BLACK_WON, game.state());
+  }
+
+  @Test
   void makingAValidMoveShouldUpdateTheMovedPiece() {
     Pos from = new Pos("b1");
     Pos to = new Pos("c3");
